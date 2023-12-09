f9561fa6f3b815f5982a50db0cfc225a59165950
ikok07 <kokmarok@gmail.com>
2023-12-09 20:10:59 +0200

    Initial Commit

---
+++ BasicAppTemplate/BasicAppTemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicAppTemplateApp.swift
+//  BasicAppTemplate
+//
+//  Created by Kaloyan Petkov on 9.12.23.
+//
+
+import SwiftUI
+
+@main
+struct BasicAppTemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BasicAppTemplate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BasicAppTemplate
+//
+//  Created by Kaloyan Petkov on 9.12.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
