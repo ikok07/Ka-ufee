f8efa932cdcea19ca4d15bc0ba55b6a865847c85
ikok07 <kokmarok@gmail.com>
2023-12-12 22:08:58 +0200

    develop forgot password

---
+++ BasicAppTemplate/View/Before Authentication/Forgot password/ForgotPasswordSuccessfullyChangedView.swift
@@ -0,0 +1,18 @@
+//
+//  ForgotPasswordSuccessfullyChangedView.swift
+//  BasicAppTemplate
+//
+//  Created by Kaloyan Petkov on 12.12.23.
+//
+
+import SwiftUI
+
+struct ForgotPasswordSuccessfullyChangedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ForgotPasswordSuccessfullyChangedView()
+}
